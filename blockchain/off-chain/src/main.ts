8da45acbdc118d2de6697dd490f4ad3836a2d9a8
David3Emmanuel <david3emmanuel@gmail.com>
2025-05-30 01:37:00 +0100

    Add Swagger integration and validation pipes

--- blockchain/off-chain/src/main.ts
+++ blockchain/off-chain/src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
+import { Logger } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // Automatically transform payloads to DTO instances
+      whitelist: true, // Strip properties that do not have decorators
+      forbidNonWhitelisted: true, // Throw error on unexpected properties
+    }),
+  )
+
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(process.env.PORT ?? 3000)
 }
-bootstrap()
+
+bootstrap().catch((error) => {
+  Logger.error('Error during bootstrap:', error)
+  process.exit(1) // Exit the process with a failure code
+})
